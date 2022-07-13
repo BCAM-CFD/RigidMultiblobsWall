9fa68b64f707da3085ad175b7e80a37b80599bb4
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2022-07-13 07:56:29 +0200

    comments removed

--- multi_bodies/examples/map/user_defined_functions.py
+++ multi_bodies/examples/map/user_defined_functions.py
@@ -21,7 +21,6 @@ def set_slip_by_ID_new(body, slip, *args, **kwargs):
   See the example in
   "examples/pair_active_rods/".
   '''
-  print('set_slip_by_ID_new!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
   if slip is not None:
     active_body_slip_partial = partial(active_body_slip_new, slip = slip)
     body.function_slip = active_body_slip_partial
@@ -41,7 +40,6 @@ def active_body_slip_new(body, slip):
   This function can be used, for example, to model active rods
   that propel along their axes. 
   '''
-  # print('active_body_slip!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
   # Get rotation matrix
   rotation_matrix = body.orientation.rotation_matrix()
 
@@ -71,7 +69,6 @@ def get_ghost_blobs_forces(bodies):
   blob_forces = [] 
   for b in bodies:
     if hasattr(b, 'ghost_reference_forces'):
-      print('hasattr(b, ghost_reference_forces): = ', hasattr(b, 'ghost_reference_forces'))
       blob_forces.append(np.dot(b.ghost_reference_forces, b.orientation.rotation_matrix().T))
   return np.vstack(blob_forces) if len(blob_forces) > 0 else np.zeros(0)
 
@@ -97,7 +94,6 @@ def calc_slip_new(bodies, Nblobs, *args, **kwargs):
   '''
   Function to calculate the slip in all the blobs.
   '''
-  print('calc_slip!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
   slip = np.zeros((Nblobs, 3))
   
   # Add prescribed slip 
@@ -108,30 +104,17 @@ def calc_slip_new(bodies, Nblobs, *args, **kwargs):
 
   # Get ghost blobs forces
   blob_forces_ghosts = get_ghost_blobs_forces(bodies)
-  print('blob_forces_ghosts = ', blob_forces_ghosts[0])
  
   # Calc flow perturbations
   if blob_forces_ghosts.size > 0:
-    print('if blob_forces_ghosts.size > 0: ', blob_forces_ghosts.size)
     # Get ghost and real blobs
     r_vectors = get_blobs_r_vectors(bodies, Nblobs)
     r_vectors_ghosts = get_ghost_blobs_r_vectors(bodies)
-    print('r_vectors = ', r_vectors[0])
-    print('r_vectors_ghosts = ', r_vectors_ghosts[0])
     
     # Get blob radius
     radius_blobs = get_blobs_radius(bodies)
     radius_blobs_ghost = get_ghosts_blobs_radius(bodies)
-
     eta = kwargs.get('eta')
-
-    print('kwargs.get(wall) = ', kwargs.get('wall'))
-    print('implementation   = ', kwargs.get('implementation'))
-    print('wall             = ', kwargs.get('implementation').find('no_wall'))
-    print('periodic         = ', kwargs.get('periodic_length'))
-    print('radius_blobs     = ', radius_blobs.shape, radius_blobs[0:2])
-    print('eta              = ', eta)
-    print('\n\n\n')
 
     slip = slip.flatten()
     if kwargs.get('implementation').find('no_wall') == -1:
@@ -139,14 +122,9 @@ def calc_slip_new(bodies, Nblobs, *args, **kwargs):
                                                                              radius_blobs_ghost, radius_blobs, eta,
                                                                              periodic_length=kwargs.get('periodic_length'))
     else:
-      print('AAA')
-      print('slip = ', np.linalg.norm(slip))
       slip -= mob.no_wall_mobility_trans_times_force_source_target_numba(r_vectors_ghosts, r_vectors, blob_forces_ghosts,
                                                                          radius_blobs_ghost, radius_blobs, eta,
                                                                          periodic_length=kwargs.get('periodic_length'))
-      print('slip = ', np.linalg.norm(slip))
-
-      
   return slip
 multi_bodies_functions.calc_slip = calc_slip_new
 
@@ -157,15 +135,10 @@ def bodies_external_force_torque_new(bodies, r_vectors, *args, **kwargs):
   It returns an array with shape (2*len(bodies), 3)
   '''
   force_torque = np.zeros((2*len(bodies), 3))
-  print('bodies_external_force_torque_new !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
   for k, b in enumerate(bodies):
     if hasattr(b, 'ghost_force_torque'):
-      print('if hasattr(b, \'ghost_force_torque\'): ', hasattr(b, 'ghost_force_torque'), k)
       force_torque[2*k] = -np.dot(b.ghost_force_torque[0], b.orientation.rotation_matrix().T)
       force_torque[2*k + 1] = -np.dot(b.ghost_force_torque[1], b.orientation.rotation_matrix().T)
-      print('W0 = ', np.linalg.norm(np.dot(b.ghost_force_torque[0], b.orientation.rotation_matrix().T) - np.dot(b.orientation.rotation_matrix(), b.ghost_force_torque[0])))
-      print('W1 = ', np.linalg.norm(np.dot(b.ghost_force_torque[1], b.orientation.rotation_matrix().T) - np.dot(b.orientation.rotation_matrix(), b.ghost_force_torque[1])))
-      
   return force_torque
 multi_bodies_functions.bodies_external_force_torque = bodies_external_force_torque_new
